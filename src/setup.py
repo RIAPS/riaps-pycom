d3f49f54a0b4d2709e24d2ae6f83cffee379b53d
Abhishek Dubey <dabhishe@isis.vanderbilt.edu>
2016-12-06 04:40:22 +0100

    installing the keys, the configuration files, and other riaps scipts

--- src/setup.py
+++ src/setup.py
@@ -8,6 +8,14 @@ setup(name='riaps',
       author_email='riaps@vanderbilt.com',
       license='',
       packages=find_packages(),
+      data_files=[('riaps/etc', ['config/riaps.conf']),
+                  ('riaps/lang',['riaps/lang/riaps.tx', 'riaps/lang/depl.tx']),
+                  ('riaps/keys',['keys/id_rsa.key','keys/id_rsa.pub'])
+               ],
+      scripts = [
+        'scripts/riaps_actor',"scripts/riaps_ctrl","scripts/riaps_deplo", "scripts/riaps_disco","scripts/riaps_lang"
+
+     ],
       install_requires=['rpyc>=3.4', 'netifaces', 'textX >= 1.4','redis >= 2.10.5','hiredis >= 0.2.0','pyzmq >= 16','pycapnp >= 0.5.9','netifaces >= 0.10.5','paramiko >= 2.0.2','cryptography >= 1.5.3'],
 dependency_links=[
         "git+https://github.com/tomerfiliba/rpyc#egg=rpyc-3.4"
