abc53cc9ea56b6c6c27cc1de530d2f0dbb21b7bf
Abhishek Dubey <dabhishe@isis.vanderbilt.edu>
2016-12-30 12:18:56 -0600

    bumped the version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='riaps',
-      version='0.1',
+      version='0.2',
       description='python implementation of riaps component model',
       url='',
       author='riaps',
