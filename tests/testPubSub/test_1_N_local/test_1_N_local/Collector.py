71a3a68c9f7ba21b08c5d78f4e4e4b1d1ef47438
Istvan <istvan.madari#vanderbilt.edu>
2017-01-12 17:02:22 -0600

    added: init of test_1_N pubsub local

---
+++ tests/testPubSub/test_1_N_local/test_1_N_local/Collector.py
@@ -0,0 +1,23 @@
+# import riaps
+from riaps.run.comp import Component
+import logging
+
+class Collector(Component):
+    def __init__(self):
+        super(Collector, self).__init__()
+        self.testlogger = logging.getLogger(__name__)
+        self.testlogger.setLevel(logging.DEBUG)
+        self.fh = logging.FileHandler('/tmp/test_1_N_ActorTest1Ns.log')
+        self.fh.setLevel(logging.DEBUG)
+        self.testlogger.addHandler(self.fh)
+        self.messageCounter = 0
+        
+    def on_getTemperature(self):
+        msg = self.getTemperature.recv_pyobj()
+        self.testlogger.info("Received messages: %d", msg)
+
+    
+
+
+
+
---
+++ tests/testPubSub/test_1_N_local/test_1_N_local/TemperatureSensor.py
@@ -0,0 +1,31 @@
+#
+import signal
+
+from riaps.run.comp import Component
+import logging
+import os
+import sys
+
+class TemperatureSensor(Component):
+    def __init__(self):
+        super(TemperatureSensor, self).__init__()
+        self.pid = os.getpid()
+        self.pending = 0
+
+        self.testlogger = logging.getLogger(__name__)
+        self.testlogger.setLevel(logging.DEBUG)
+        self.fh = logging.FileHandler('/tmp/test_1_N_ActorTest1Np.log')
+        self.fh.setLevel(logging.DEBUG)
+        self.testlogger.addHandler(self.fh)
+        self.messageCounter = 0
+
+    def on_clock(self):
+        msg = self.clock.recv_pyobj()
+        #self.testlogger.info("[%d] on_clock():%s [%d]", msg, self.pid)
+        self.messageCounter += 1
+        msg = self.messageCounter
+        self.sendTemperature.send_pyobj(msg)
+        self.testlogger.info("Sent messages: %d", self.messageCounter)
+        if self.messageCounter == 15:
+            print("kill %d", os.getpid())
+            os.kill(os.getpid(), signal.SIGTERM)
