94b0e2f06c60261f823f2cfd5612a366338dfae0
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-01-12 18:24:55 -0600

    fixed errors : removed riaps_init as it did not exist. rpyc_registry is installed with rpyc

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='riaps',
-      version='0.2',
+      version='0.3',
       description='python implementation of riaps component model',
       url='',
       author='riaps',
@@ -18,7 +18,7 @@ setup(name='riaps',
                   ('riaps/keys',['riaps/keys/id_rsa.key','riaps/keys/id_rsa.pub'])
                ],
       scripts = [
-        'scripts/riaps_actor',"scripts/riaps-dev.node","scripts/rpyc_registry", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps_init","scripts/riaps-dev.ctrl","scripts/riaps_init","scripts/sshd.py","scripts/riaps-dev.mn"
+        'scripts/riaps_actor',"scripts/riaps-dev.node", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps-dev.ctrl","scripts/sshd.py","scripts/riaps-dev.mn"
 
      ],
       install_requires=['rpyc>=3.3.1','textX >= 1.4','redis >= 2.10.5','hiredis >= 0.2.0','pyzmq >= 16','pycapnp >= 0.5.9','netifaces >= 0.10.5','paramiko >= 2.0.2','cryptography >= 1.5.3'],
