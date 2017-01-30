e436d8327abe4aa42374b2201b1a94bf76a86918
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2017-01-30 15:09:58 -0600

    Move to riaps-pycom v0.3.2 - adding device manager capability

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='riaps',
-      version='0.3.1',
+      version='0.3.2',
       description='python implementation of riaps component model',
       url='',
       author='riaps',
@@ -18,7 +18,7 @@ setup(name='riaps',
                   ('riaps/keys',['riaps/keys/id_rsa.key','riaps/keys/id_rsa.pub'])
                ],
       scripts = [
-        'scripts/riaps_actor',"scripts/riaps-dev.node", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps-dev.ctrl","scripts/sshd.py","scripts/riaps-dev.mn","scripts/riaps_device","riaps_devm"
+        'scripts/riaps_actor',"scripts/riaps-dev.node", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps-dev.ctrl","scripts/sshd.py","scripts/riaps-dev.mn","scripts/riaps_device","scripts/riaps_devm"
 
      ],
       install_requires=['rpyc>=3.3.1','textX >= 1.4','redis >= 2.10.5','hiredis >= 0.2.0','pyzmq >= 16','pycapnp >= 0.5.9','netifaces >= 0.10.5','paramiko >= 2.0.2','cryptography >= 1.5.3'],
