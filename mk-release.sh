9dff2d8a27d3748e10aee3778a609d24bd88abf3
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-03-27 20:45:37 -0500

    Update mk-release.sh

--- mk-release.sh
+++ mk-release.sh
@@ -11,6 +11,6 @@ pwd
 git clone file:///$pycomrepo 
 sed -i s/@version@/$version/g riaps-pycom/src/setup.py
 cat riaps-pycom/src/setup.py
-tar cvzf riaps-pycom_v$version.tar.gz riaps-pycom
-cp riaps-pycom_v$version.tar.gz $pycomrepo/.
+tar cvzf riaps-pycom.tar.gz riaps-pycom
+cp riaps-pycom.tar.gz $pycomrepo/.
 rm -rf $tmpdir 
