205b024d6881f71d7c7bb4ed91b0541c45c69665
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-05-23 10:10:02 -0500

    updated version and set the flags for pycapnp correctly

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-export externalsversion="0.4.2" 
-export coreversion="0.4.3"
-export pycomversion="0.4.2"
+export externalsversion="0.4.3" 
+export coreversion="0.4.4"
+export pycomversion="0.4.3"
 
