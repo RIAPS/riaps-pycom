f13cae4c2aa06513a18f0a625b683a219fc21675
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-05-24 20:37:12 -0500

    1. added code to copy gpio rules (armhf) 2. added postint rule for armhf to create gpio group if it does not exist 3. added dialout group for armhf to postint (will create only if the dialout group does not already exist) 4. Added udevadm trigger --subsystem-match=gpio  (to postinst for armhf) 5. added the following rules to /etc/sudoers.d/riaps
    Defaults    env_keep += SLOTS
    Defaults    env_keep += PINS
    
    6. pip3 install pyserial minimalmodbus (AdaFruitBBIO is already installs for armhf)
    7. removed Adafruit BBIO for amd64 install
    8.  added the following to profile.d for armhf
    
    export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
    
    and export SLOTS=/sys/devices/platform/bone_capemgr/slots
    
    9. Added resolv.conf for armhf the postinst instruction installs it and saves the older version
    
    10. Added interfaces for armhf

--- services/armhf/etc/profile.d/20-riaps-env.sh
+++ services/armhf/etc/profile.d/20-riaps-env.sh
@@ -1,1 +1,6 @@
 for i in `cat /etc/riaps/env.conf`;do export $i ;done
+# RIAPS Device Slots
+export SLOTS=/sys/devices/platform/bone_capemgr/slots
+# RIAPS Device Pins
+export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
+
