6adce9cd207d66d2bacf6d7945c3f26b66152025
Mary Metelko <mary@metelkos-MacBook-Pro.local>
2017-05-30 16:18:20 -0500

    Update which scripts get installed, removed mininet related scripts

--- src/setup.py
+++ src/setup.py
@@ -18,7 +18,7 @@ setup(name='riaps-pycom',
                   ('riaps/keys',['riaps/keys/id_rsa.key','riaps/keys/id_rsa.pub'])
                ],
       scripts = [
-        'scripts/riaps_actor',"scripts/riaps-dev.node", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps-dev.ctrl","scripts/sshd.py","scripts/riaps-dev.mn","scripts/riaps_device","scripts/riaps_devm"
+        "scripts/riaps_actor", "scripts/riaps_ctrl", "scripts/riaps_deplo", "scripts/riaps_disco", "scripts/riaps_lang", "scripts/riaps_depll", "scripts/riaps_device", "scripts/riaps_devm"
 
      ],
       install_requires=['rpyc>=3.3.1','textX >= 1.4','redis >= 2.10.5','hiredis >= 0.2.0','pyzmq >= 16','pycapnp >= 0.5.9','netifaces >= 0.10.5','paramiko >= 2.0.2','cryptography >= 1.5.3', 'Adafruit_BBIO >= 1.0.3'],
