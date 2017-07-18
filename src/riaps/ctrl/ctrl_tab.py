d6791de55de3debacbfe47cda9aaab0d3679d536
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-07-17 20:41:37 -0500

    adding check to see if key exists or not (#24)
    * adding check to see if key exists or not
    
    * updated to use os.path.expanduser
    
    * addressing comments
    
    * fixed incorrect usage of expand user
    
    * Removed ssh key management

--- src/riaps/ctrl/ctrl_tab.py
+++ src/riaps/ctrl/ctrl_tab.py
@@ -182,11 +182,11 @@ class Controller_Tab(object):
         # get host key, if we know one
         self.hostKeys = {}
         try:
-            self.hostKeys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+            self.hostKeys = paramiko.util.load_host_keys(os.path.expanduser(os.path.join('~','.ssh','known_hosts')))
         except IOError:
             try:
                 # try ~/ssh/ too, e.g. on windows
-                self.hostKeys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+                self.hostKeys = paramiko.util.load_host_keys(os.path.expanduser(os.path.join('~','ssh','known_hosts')))
             except IOError:
                 pass     
 
@@ -196,7 +196,13 @@ class Controller_Tab(object):
         keys available from an SSH agent or from a local private RSA key file (assumes no pass phrase).
         """
         rsa_private_key = join(self.riaps_Folder,"keys/" + str(const.ctrlPrivateKey))
-        
+        # check if the rsa_private_key exists or not
+        if not os.path.isfile(rsa_private_key):
+           self.logger.warning('cannot find key %s. Looking in different location' %(rsa_private_key))
+           #set the private key to /home/riaps/.ssh/
+           rsa_private_key = os.path.expanduser(os.path.join('~','.ssh',str(const.ctrlPrivateKey)))
+           if not os.path.isfile(rsa_private_key):
+             self.logger.error('cannot find key %s' %(rsa_private_key))
         try:
             ki = paramiko.RSAKey.from_private_key_file(rsa_private_key)
         except Exception as e:
