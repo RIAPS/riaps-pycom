e672f11a070ac62d015ae6c5de849c853fcfdcbf
MMetelko <MMetelko@users.noreply.github.com>
2017-11-09 16:21:08 -0600

    Put in timing instrumentations and update port to specify 'UARTX'

--- tests/ModbusUartReqRepTesting/ComputationalComponent.py
+++ tests/ModbusUartReqRepTesting/ComputationalComponent.py
@@ -10,7 +10,12 @@ import os
 from collections import namedtuple
 from ModbusUartDevice import CommandFormat,ModbusCommands
 import pydevd
+import time
+import logging
 
+
+''' Enable debugging to gather timing information on the code execution'''
+debugMode = True
 
 RegSet = namedtuple('RegSet', ['idx', 'value'])
 InputRegs = namedtuple('InputRegs', ['outputCurrent','outputVolt','voltPhase','time'])
@@ -21,7 +26,7 @@ HoldingRegs = namedtuple('HoldingRegs',['unused', 'startStopCmd', 'power'])
 1.start/stop, 2.power command, 3. frequency shift from secondary control, 4. voltage magnitude shift from secondary control.
 HoldingRegs = namedtuple('HoldingRegs',['startStopCmd', 'powerCmd', 'freqShift', 'voltMagShift']) 
 '''
- 
+
 class ComputationalComponent(Component):
     def __init__(self):
         super().__init__()
@@ -30,7 +35,13 @@ class ComputationalComponent(Component):
         self.pid = os.getpid()
         self.inputRegs = InputRegs(RegSet(0,45),RegSet(1,56),RegSet(2,78),RegSet(3,91))
         self.holdingRegs = HoldingRegs(RegSet(0,0),RegSet(1,55),RegSet(2,66))
-        
+
+        if debugMode:
+            self.logger.setLevel(logging.DEBUG)
+            self.logger.handlers[0].setLevel(logging.DEBUG) # a workaround for hardcoded INFO level of StreamHandler logger
+        else:
+            self.logger.setLevel(logging.INFO)
+
         '''Setup Commands'''
         self.defaultNumOfRegs = 1
         self.dummyValue = [0]
@@ -39,20 +50,20 @@ class ComputationalComponent(Component):
         self.ModbusPending = 0
 
         self.logger.info("ComputationalComponent: %s - starting",str(self.pid)) 
-               
+
 
     def on_clock(self):
         now = self.clock.recv_pyobj() 
         self.logger.info("on_clock()[%s]: %s",str(self.pid),str(now))
 
         '''Request:  Commands to send over Modbus - one command used at a time'''
-        
+
         '''Read/Write (holding only) a single register'''
-        #self.command = CommandFormat(ModbusCommands.READ_INPUTREG,self.inputRegs.time.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault) 
+        #self.command = CommandFormat(ModbusCommands.READ_INPUTREG,self.inputRegs.time.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
         #self.command = CommandFormat(ModbusCommands.READ_HOLDINGREG,self.holdingRegs.startStopCmd.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
         #self.values = [83]
         #self.command = CommandFormat(ModbusCommands.WRITE_HOLDINGREG,self.holdingRegs.power.idx,self.defaultNumOfRegs,self.values,self.defaultNumOfDecimals,self.signedDefault)
-       
+
         '''Read/Write all input registers'''
         #numRegsToRead = len(self.inputRegs)
         #self.command = CommandFormat(ModbusCommands.READMULTI_INPUTREGS,self.inputRegs.outputCurrent.idx,numRegsToRead,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
@@ -61,9 +72,13 @@ class ComputationalComponent(Component):
         self.command = CommandFormat(ModbusCommands.READMULTI_HOLDINGREGS,self.holdingRegs.unused.idx,len(self.holdingRegs),self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
         #self.values = [75,67]
         #self.command = CommandFormat(ModbusCommands.WRITEMULTI_HOLDINGREGS,self.holdingRegs.startStopCmd.idx,2,self.values,self.defaultNumOfDecimals,self.signedDefault)
-         
+
+        if debugMode:
+            self.cmdSendStartTime = time.perf_counter()
+            self.logger.debug("on_clock()[%s]: Send command to ModbusUartDevice at %f",str(self.pid),self.cmdSendStartTime)
+
         msg = self.command
-        self.logger.info('[%d] send req: %s' % (self.pid,msg))
+        self.logger.info('on_clock()[%d]: send req: %s' % (self.pid,msg))
         if self.modbusReqPort.send_pyobj(msg):
             self.ModbusPending += 1
               
@@ -72,8 +87,12 @@ class ComputationalComponent(Component):
             msg = self.modbusReqPort.recv_pyobj()
             self.logger.info("on_modbusReqPort()[%s]: %s",str(self.pid),repr(msg))
             self.ModbusPending -= 1
-#           pydevd.settrace(host='192.168.1.102',port=5678)  
-        
+#           pydevd.settrace(host='192.168.1.102',port=5678)
+
+            if debugMode:
+                self.cmdResultsRxTime = time.perf_counter()
+                self.logger.debug("on_clock()[%s]: Received Modbus data from ModbusUartDevice at %f, time from cmd to data is %f ms",str(self.pid),self.cmdResults$
+
             if self.command.commandType == ModbusCommands.READ_INPUTREG or self.command.commandType == ModbusCommands.READ_HOLDINGREG:
                 logMsg = "Register " + str(self.command.registerAddress) + " value is " + str(msg)
             elif self.command.commandType == ModbusCommands.READMULTI_INPUTREGS or self.command.commandType == ModbusCommands.READMULTI_HOLDINGREGS:
@@ -82,11 +101,8 @@ class ComputationalComponent(Component):
                 logMsg = "Wrote Register " + str(self.command.registerAddress)
             elif self.command.commandType == ModbusCommands.WRITEMULTI_HOLDINGREGS:
                 logMsg = "Wrote Registers " + str(self.command.registerAddress) + " to " + str(self.command.registerAddress + self.command.numberOfRegs - 1)
-            
+
             self.tx_modbusData.send_pyobj(logMsg)  # Send log data
-        
+
     def __destroy__(self):
         self.logger.info("[%d] destroyed" % self.pid)
-        
-        
-        
--- tests/ModbusUartReqRepTesting/ModbusUartDevice.py
+++ tests/ModbusUartReqRepTesting/ModbusUartDevice.py
@@ -16,7 +16,11 @@ from serialModbusLib.serialModbusComm import SerialModbusComm,PortConfig
 from collections import namedtuple
 from enum import Enum
 #import pydevd
+import time
 
+
+''' Enable debugging to gather timing information on the code execution'''
+debugMode = True
 
 class ModbusCommands(Enum):
     READ_BIT = 1
@@ -29,13 +33,33 @@ class ModbusCommands(Enum):
     WRITEMULTI_HOLDINGREGS = 8
 
 CommandFormat = namedtuple('CommandFormat', ['commandType','registerAddress','numberOfRegs','values','numberOfDecimals','signedValue'])
-    
+
 class ModbusUartDevice(Component):
-    def __init__(self,slaveaddress=0,port="ttyO1",baudrate=19200,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,serialTimeout=0.05): # defaults for Modbus spec
+    def __init__(self,slaveaddress=0,port="UART2",baudrate=19200,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,serialTimeout=0.05): # defaults for Modbus spec
         super().__init__()
-        self.logger.setLevel(logging.DEBUG)
+        if debugMode:
+            self.logger.setLevel(logging.DEBUG)
+            self.logger.handlers[0].setLevel(logging.DEBUG) # a workaround for hardcoded INFO level of StreamHandler logger
+        else:
+            self.logger.setLevel(logging.INFO)
+
         self.pid = os.getpid()
-        self.port_config = PortConfig("/dev/" + port, baudrate, bytesize, parity, stopbits, serialTimeout)        
+
+        if port == 'UART1':
+            self.port = '/dev/ttyO1'
+        elif port == 'UART2':
+            self.port = '/dev/ttyO2'
+        elif port == 'UART3':
+            self.port = '/dev/ttyO3'
+        elif port == 'UART4':
+            self.port = '/dev/ttyO4'
+        elif port == 'UART5':
+            self.port = '/dev/ttyO5'
+        else:
+            self.logger.error('__init__[%s]: Invalid UART argument, use UART1..5', self.pid)
+            sys.exit(-1)
+
+        self.port_config = PortConfig(self.port, baudrate, bytesize, parity, stopbits, serialTimeout)
         self.slaveAddressDecimal = slaveaddress
         self.modbus = SerialModbusComm(self,self.slaveAddressDecimal,self.port_config)
         self.modbusReady = False
@@ -43,35 +67,56 @@ class ModbusUartDevice(Component):
 
     def on_clock(self):
         now = self.clock.recv_pyobj()   # Receive time (as float)
-        self.logger.info("on_clock()[%s]: %s",str(self.pid),now)        
-        
+        self.logger.info("on_clock()[%s]: %s",str(self.pid),now)
+
+        if debugMode:
+            t0 = time.perf_counter()
+            self.logger.debug("on_clock()[%s]: Request Modbus start at %f",str(self.pid),t0)
+
         if self.modbusReady == False:
             self.modbus.startModbus()
 #            pydevd.settrace(host='192.168.1.102',port=5678)
             self.modbusReady = True
-                    
+
+        if debugMode:
+            t1 = time.perf_counter()
+            self.logger.debug("on_clock()[%s]: Modbus ready at %f, time to start Modbus is %f ms",str(self.pid),t1,(t1-t0)*1000)
+
+        self.clock.halt()
+
+
     def __destroy__(self):
         self.logger.info("__destroy__")
         self.modbus.stopModbus()
-        
+
+
     '''    
     Receive a Modbus command request.  Process command and send back response.
     '''
-    def on_modbusRepPort(self):                
-        '''Request Received'''         
-        commandRequest = self.modbusRepPort.recv_pyobj()     
-        self.logger.info("on_modbusRepPort()[%s]: %s",str(self.pid),commandRequest) 
+    def on_modbusRepPort(self):
+        '''Request Received'''
+        commandRequest = self.modbusRepPort.recv_pyobj()
+        self.logger.info("on_modbusRepPort()[%s]: %s",str(self.pid),commandRequest)
+
+        if debugMode:
+            self.modbusReqRxTime = time.perf_counter()
+            self.logger.debug("on_modbusRepPort()[%s]: Request Received at %f",str(self.pid),self.modbusReqRxTime)
 
         self.unpackCommand(commandRequest)
-        responseValue = -1  # invalid response 
+        responseValue = -1  # invalid response
         if self.modbusReady == True:
             responseValue = self.sendModbusCommand()
-            
+
+            if debugMode:
+                t1 = time.perf_counter()
+                self.logger.debug("on_modbusRepPort()[%s]: Send command to Thread at %f",str(self.pid),t1)
+
         '''Send Results'''
         self.modbusRepPort.send_pyobj(responseValue)
 #        pydevd.settrace(host='192.168.1.102',port=5678)
-        self.logger.info("on_modbusRepPort()[%s]: %s,",str(self.pid),responseValue)        
-        
+        self.logger.info("on_modbusRepPort()[%s]: %s,",str(self.pid),responseValue)
+
+
     def unpackCommand(self,rxCommand):
         self.commmandRequested = rxCommand.commandType
         self.registerAddress = rxCommand.registerAddress
@@ -79,10 +124,15 @@ class ModbusUartDevice(Component):
         self.numberOfDecimals = rxCommand.numberOfDecimals
         self.signedValue = rxCommand.signedValue
         self.values = rxCommand.values
-        
+
+
     def sendModbusCommand(self):
         value = 999  # large invalid value
-        
+
+        if debugMode:
+            t0 = time.perf_counter()
+            self.logger.debug("sendModbusCommand()[%s]: Sending command to Modbus library at %f",str(self.pid),t0)
+
         if self.commmandRequested == ModbusCommands.READ_INPUTREG:
             value = self.modbus.readInputRegValue(self.registerAddress, self.numberOfDecimals, self.signedValue)
             self.logger.info("ModbusUartDevice: sent command %s, register=%d, numOfDecimals=%d, signed=%s", ModbusCommands.READ_INPUTREG.name,self.registerAddress,self.numberOfDecimals,str(self.signedValue))
@@ -101,6 +151,11 @@ class ModbusUartDevice(Component):
         elif self.commmandRequested == ModbusCommands.WRITEMULTI_HOLDINGREGS:
             self.modbus.writeHoldingRegisters(self.registerAddress, self.values)
             self.logger.info("ModbusUartDevice: sent command %s, register=%d",ModbusCommands.WRITEMULTI_HOLDINGREGS.name,self.registerAddress)
-            self.logger.info("ModbusUartDevice: Values - %s", str(self.values).strip('[]'))        
+            self.logger.info("ModbusUartDevice: Values - %s", str(self.values).strip('[]'))
+
+        if debugMode:
+            t1 = time.perf_counter()
+            self.logger.debug("sendModbusCommand()[%s]: Modbus library command complete at %f, time to interact with Modbus library is %f ms",str(self.pid),t1$
+
         return value
-        
+
