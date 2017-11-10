8a5a1b5d72b8dc5dd92bb09a2a33d6681416fd08
MMetelko <MMetelko@users.noreply.github.com>
2017-11-10 15:21:06 -0600

    First draft of polling, pushing to allow testing on VM

--- tests/ModbusUartPollTesting/ComputationalComponent.py
+++ tests/ModbusUartPollTesting/ComputationalComponent.py
@@ -8,7 +8,7 @@ from riaps.run.comp import Component
 import uuid
 import os
 from collections import namedtuple
-from ModbusUartDevice import ModbusRequest,ModbusCommands,RequestFormat,CommandFormat
+from ModbusUartPollDevice import ModbusRequest,ModbusCommand,RequestFormat,CommandFormat
 import pydevd
 import time
 import logging
@@ -21,6 +21,7 @@ InputRegs = namedtuple('InputRegs', ['outputCurrent','outputVolt','voltPhase','t
 
 '''For Inverter control'''
 HoldingRegs = namedtuple('HoldingRegs',['unused', 'startStopCmd', 'power'])
+# MM TODO:  add 'reactive_power' after initial measurement comparison - HoldingRegs = namedtuple('HoldingRegs',['unused', 'startStopCmd', 'power','reactive_power'])
 '''For RIAPS future
 1.start/stop, 2.power command, 3. frequency shift from secondary control, 4. voltage magnitude shift from secondary control.
 HoldingRegs = namedtuple('HoldingRegs',['startStopCmd', 'powerCmd', 'freqShift', 'voltMagShift']) 
@@ -48,8 +49,9 @@ class ComputationalComponent(Component):
         self.signedDefault = False
         self.modbusPending = 0
         self.pollRequested = False
+        self.pollFreq = 0
         self.dataExpected = False
-        self.pollCmdList = []
+        self.noData = None
 
         self.logger.info("__init__[%s]: starting",str(self.pid))
 
@@ -60,54 +62,58 @@ class ComputationalComponent(Component):
         '''Request:  Query Commands to send over Modbus - one command used at a time, options to test below'''
 
         '''Read/Write (holding only) a single register'''
-        #Option1: self.command = CommandFormat(ModbusCommands.READ_INPUTREG,self.inputRegs.time.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault) 
-        #Option2: self.command = CommandFormat(ModbusCommands.READ_HOLDINGREG,self.holdingRegs.startStopCmd.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
+        #Option1: self.command = CommandFormat(ModbusCommand.READ_INPUTREG,self.inputRegs.time.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
+        #Option2: self.command = CommandFormat(ModbusCommand.READ_HOLDINGREG,self.holdingRegs.startStopCmd.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
         #Option3: self.values = [83]
-        #         self.command = CommandFormat(ModbusCommands.WRITE_HOLDINGREG,self.holdingRegs.power.idx,self.defaultNumOfRegs,self.values,self.defaultNumOfDecimals,self.signedDefault)
+        #         self.command = CommandFormat(ModbusCommand.WRITE_HOLDINGREG,self.holdingRegs.power.idx,self.defaultNumOfRegs,self.values,self.defaultNumOfDecimals,self.signedDefault)
 
         '''Read/Write all input registers'''
         #Option4: numRegsToRead = len(self.inputRegs)
-        #         self.command = CommandFormat(ModbusCommands.READMULTI_INPUTREGS,self.inputRegs.outputCurrent.idx,numRegsToRead,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
+        #         self.command = CommandFormat(ModbusCommand.READMULTI_INPUTREGS,self.inputRegs.outputCurrent.idx,numRegsToRead,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
 
         '''Read/Write all holding registers'''
         #Option5:
-        self.command = CommandFormat(ModbusCommands.READMULTI_HOLDINGREGS,self.holdingRegs.unused.idx,len(self.holdingRegs),self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
+        self.command = CommandFormat(ModbusCommand.READMULTI_HOLDINGREGS,self.holdingRegs.unused.idx,len(self.holdingRegs),self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
 
         #Option6: self.values = [75,67]
-        #         self.command = CommandFormat(ModbusCommands.WRITEMULTI_HOLDINGREGS,self.holdingRegs.startStopCmd.idx,2,self.values,self.defaultNumOfDecimals,self.signedDefault)
+        #         self.command = CommandFormat(ModbusCommand.WRITEMULTI_HOLDINGREGS,self.holdingRegs.startStopCmd.idx,2,self.values,self.defaultNumOfDecimals,self.signedDefault)
 
         '''Setup Request - requestType and requestData '''
-        self.requestCommand = RequestFormat(ModbusRequest.QUERY_MODBUS,self.command)
+        self.requestCommand = RequestFormat(ModbusRequest.SINGLE_RW_CMD,self.command)
+
+        self.sendModbusRequest()
 
         '''Polling Setup Command'''
-        #command1 = CommandFormat(ModbusCommands.READ_HOLDINGREG,self.holdingRegs.startStopCmd.idx,self.defaultNumOfRegs,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
-        #self.pollCmdList.append(command1)
+        #readCommand = CommandFormat(ModbusCommand.READMULTI_INPUTREGS,self.inputRegs.outputCurrent.idx,numRegsToRead,self.dummyValue,self.defaultNumOfDecimals,self.signedDefault)
+        #writeCommand = CommandFormat(ModbusCommand.WRITE_HOLDINGREG,self.holdingRegs.power.idx,self.defaultNumOfRegs,value,self.defaultNumOfDecimals,self.signedDefault)
+        #self.pollFreq = 25 # 25 Hz
+        #self.requestCommand = RequestFormat(ModbusRequest.SETUP_POLLING,SamplingRequest(readCommand,writeCommand,self.pollFreq))
+
+        '''Setup Polling Write Value'''
         #value = [83]
-        #command2 = CommandFormat(ModbusCommands.WRITE_HOLDINGREG,self.holdingRegs.power.idx,self.defaultNumOfRegs,value,self.defaultNumOfDecimals,self.signedDefault)
-        #self.pollCmdList.append(command2)
-        #self.requestCommand = RequestFormat(ModbusRequest.SETUP_POLLER,self.pollCmdList) 
+        #self.requestCommand = RequestFormat(ModbusRequest.SET_POLL_WRITE_VALUE,value)
 
         '''Start Polling Command'''
-        #self.pollPeriod = 40  # in ms
-        #self.requestCommand = RequestFormat(ModbusRequest.START_POLLING,self.pollPeriod)
+        # self.requestCommand = RequestFormat(ModbusRequest.START_POLLING,self.noData)
 
         '''Stop Polling Command'''
-        #noData = None
-        #self.requestCommand = RequestFormat(ModbusRequest.STOP_POLLING,noData)
+        #self.requestCommand = RequestFormat(ModbusRequest.STOP_POLLING,self.noData)
 
+
+    def sendModbusRequest(self):
         if debugMode:
             self.cmdSendStartTime = time.perf_counter()  
-            self.logger.debug("on_clock()[%s]: Send command to ModbusUartDevice at %f",str(self.pid),self.cmdSendStartTime)
+            self.logger.debug("sendModbusRequest()[%s]: Send command to ModbusUartDevice at %f",str(self.pid),self.cmdSendStartTime)
 
         msg = self.requestCommand
-        self.logger.info("on_clock()[%d] send request: %s" % (self.pid,msg))
+        self.logger.info("sendModbusRequest()[%d] send request: %s" % (self.pid,msg))
         if self.modbusReqPort.send_pyobj(msg):
             self.modbusPending += 1
 
         '''Receive Response - ACK or ERROR'''
         if self.modbusPending > 0:
             msg = self.modbusReqPort.recv_pyobj()
-            self.logger.info("on_clock()[%s] receive response: %s",str(self.pid),repr(msg))
+            self.logger.info("sendModbusRequest()[%s] receive response: %s",str(self.pid),repr(msg))
             self.modbusPending -= 1
             # pydevd.settrace(host='192.168.1.102',port=5678)
             if msg=='ACK':
@@ -115,7 +121,8 @@ class ComputationalComponent(Component):
                 
             if debugMode:
                 self.cmdAckRxTime = time.perf_counter()     
-                self.logger.debug("on_clock()[%s]: Received ACK from ModbusUartDevice at %f, time from cmd to get ACK back is %f ms",str(self.pid),self.cmdAckRxTime,(self.cmdAckRxTime-self.cmdSendStartTime)*1000)
+                self.logger.debug("sendModbusRequest()[%s]: Received ACK from ModbusUartDevice at %f, time from cmd to get ACK back is %f ms",
+                                  str(self.pid),self.cmdAckRxTime,(self.cmdAckRxTime-self.cmdSendStartTime)*1000)
 
 
     def on_rx_modbusData(self):
@@ -125,17 +132,21 @@ class ComputationalComponent(Component):
 
         if debugMode:
             self.cmdResultsRxTime = time.perf_counter()     
-            self.logger.debug("on_rx_modbusData()[%s]: Received Modbus data from ModbusUartDevice at %f, time from cmd to data is %f ms",str(self.pid),self.cmdResultsRxTime,(self.cmdResultsRxTime-self.cmdSendStartTime)*1000)
+            self.logger.debug("on_rx_modbusData()[%s]: Received Modbus data from ModbusUartDevice at %f, time from cmd to data is %f ms",
+                              str(self.pid),self.cmdResultsRxTime,(self.cmdResultsRxTime-self.cmdSendStartTime)*1000)
 
         if self.dataExpected == True:
-            if self.command.commandType == ModbusCommands.READ_INPUTREG or self.command.commandType == ModbusCommands.READ_HOLDINGREG:
+            if self.command.commandType == ModbusCommand.READ_INPUTREG or \
+                            self.command.commandType == ModbusCommand.READ_HOLDINGREG:
                 logMsg = "Register " + str(self.command.registerAddress) + " value is " + str(msg)
-            elif self.command.commandType == ModbusCommands.READMULTI_INPUTREGS or self.command.commandType == ModbusCommands.READMULTI_HOLDINGREGS:
+            elif self.command.commandType == ModbusCommand.READMULTI_INPUTREGS or \
+                            self.command.commandType == ModbusCommand.READMULTI_HOLDINGREGS:
                 logMsg = "Register " + str(self.command.registerAddress) + " values are " + str(msg)
-            elif self.command.commandType == ModbusCommands.WRITE_HOLDINGREG:
+            elif self.command.commandType == ModbusCommand.WRITE_HOLDINGREG:
                 logMsg = "Wrote Register " + str(self.command.registerAddress)
-            elif self.command.commandType == ModbusCommands.WRITEMULTI_HOLDINGREGS:
-                logMsg = "Wrote Registers " + str(self.command.registerAddress) + " to " + str(self.command.registerAddress + self.command.numberOfRegs - 1)
+            elif self.command.commandType == ModbusCommand.WRITEMULTI_HOLDINGREGS:
+                logMsg = "Wrote Registers " + str(self.command.registerAddress) + " to " + \
+                         str(self.command.registerAddress + self.command.numberOfRegs - 1)
 
             self.tx_modbusData.send_pyobj(logMsg)  # Send log data
             self.logger.info("on_rx_modbusData()[%s]: Sent log message - %s",str(self.pid),repr(logMsg))
