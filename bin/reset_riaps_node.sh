1c8b88c6d9921557368a6f5c142871e57ccdce95
MMetelko <MMetelko@users.noreply.github.com>
2018-05-08 16:13:26 -0500

    Update Debian install and platform service

--- bin/reset_riaps_node.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for KILLPID in `ps ax | grep 'riaps_' | awk ' { print $1;}'`; do
-  sudo kill -9 $KILLPID;
-done
-
-sudo rm -R /home/riaps/riaps_apps/riaps-apps.lmdb/
-sudo rm -R /home/riaps/riaps_apps/DistributedEstimator/
-sudo userdel distributedestimator9cbd
