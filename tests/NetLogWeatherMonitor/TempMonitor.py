eba6764e3a1539538e90be43f64f7ddc0d05d81f
Joey Holliday <joeyholliday77@gmail.com>
2018-06-06 13:45:28 -0500

    Add NetworkLoggingWeatherMonitor test for demonstrating network logging

---
+++ tests/NetLogWeatherMonitor/TempMonitor.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 5, 2018
+
+@author: jeholliday
+'''
+# import riaps
+from riaps.run.comp import Component
+import logging
+import time
+import os
+import socket
+
+class TempMonitor(Component):
+    def __init__(self):
+        super(TempMonitor, self).__init__()
+        self.addNetworkLogger('vbox.local')
+        self.pid = os.getpid()
+        self.hostname = socket.gethostname()
+        self.logger.info("["+self.hostname +"]("+str(self.pid)+")-starting TempMonitor")
+        
+    def on_tempupdate(self):
+        # Receive: timestamp,temperature
+        temperatureTime, temperatureValue = self.tempupdate.recv_pyobj() 
+        self.logger.info("["+self.hostname +"]on_tempupdate(): Temperature: "+ str(temperatureValue)+", Timestamp: "+ str(temperatureTime))
+        
+    def __destroy__(self):
+        self.logger.info("["+self.hostname +"]("+str(self.pid)+")-stoping TempMonitor")
---
+++ tests/NetLogWeatherMonitor/TempSensor.py
@@ -0,0 +1,33 @@
+'''
+Created on Jun 5, 2018
+
+@author: jeholliday
+'''
+from riaps.run.comp import Component
+import logging
+import time
+import os
+import socket
+
+class TempSensor(Component):
+    def __init__(self):
+        super(TempSensor, self).__init__()
+        self.addNetworkLogger('vbox.local')
+        self.pid = os.getpid()
+        self.temperature = 65
+        self.hostname = socket.gethostname()
+        self.logger.info("["+self.hostname +"]("+str(self.pid)+")-starting TempSensor")
+        self.logger.info("["+self.hostname +"]Initial temp:"+str(self.temperature))
+        
+    def on_clock(self):
+        now = time.ctime(int(time.time()))
+        msg = self.clock.recv_pyobj()
+        self.temperature = self.temperature + 1
+        msg = str(self.temperature)
+        msg = (now,msg)       
+        self.logger.info("["+self.hostname +"]on_clock(): Temperature - " + str(msg[1]))
+        self.ready.send_pyobj(msg)
+               
+    def __destroy__(self):
+        self.logger.info("["+self.hostname +"]("+str(self.pid)+")-stopping TempSensor")         
+
