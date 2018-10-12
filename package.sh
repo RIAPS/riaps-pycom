23becf9da754676c7ced341d4e3f281d825f4a56
MMetelko <MMetelko@users.noreply.github.com>
2018-10-12 10:32:41 -0500

    Updates to move data files to Debian package install, instead of using pip install.  Will avoid overwriting the user's configuration changes.

--- package.sh
+++ package.sh
@@ -1,9 +1,30 @@
+      #data_files=[('riaps/etc', ['riaps/etc/riaps.conf',  'riaps/etc/riaps-log.conf']),
+      #            ('riaps/lang',['riaps/lang/riaps.tx', 'riaps/lang/depl.tx']),
+      #            ('riaps/etc',['riaps/etc/redis.conf']),
+      #            ('riaps/etc',['riaps/etc/riaps-ctrl.glade']),
+      #            ('riaps/keys',['riaps/keys/id_rsa.key','riaps/keys/id_rsa.pub'])
+      #         ],
+
 rm -rf package
 source version.sh
 mkdir -p package/riaps-pycom-amd64/DEBIAN
 mkdir -p package/riaps-pycom-amd64/opt/riaps-pycom/
+mkdir -p package/riaps-pycom-amd64/usr/local/riaps/etc/
+mkdir -p package/riaps-pycom-amd64/usr/local/riaps/lang/
+mkdir -p package/riaps-pycom-amd64/usr/local/riaps/keys/
+
 cp -r DEBIAN/amd64/* package/riaps-pycom-amd64/DEBIAN/.
 cp -r src package/riaps-pycom-amd64/opt/riaps-pycom/.
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-amd64/usr/local/riaps/etc/
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-amd64/usr/local/riaps/etc/
+cp -r src/riaps/etc/redis.conf package/riaps-pycom-amd64/usr/local/riaps/etc/
+cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-amd64/usr/local/riaps/etc/
+cp -r src/riaps/lang/riaps.tx package/riaps-pycom-amd64/usr/local/riaps/lang/
+cp -r src/riaps/lang/depl.tx package/riaps-pycom-amd64/usr/local/riaps/lang/
+
+cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-amd64/usr/local/riaps/keys/
+cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-amd64/usr/local/riaps/keys/
+
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-amd64/DEBIAN/control
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-amd64/opt/riaps-pycom/src/setup.py
 fakeroot dpkg-deb --build package/riaps-pycom-amd64
@@ -13,8 +34,23 @@ cp package/riaps-pycom-amd64.deb .
 source version.sh
 mkdir -p package/riaps-pycom-armhf/DEBIAN
 mkdir -p package/riaps-pycom-armhf/opt/riaps-pycom/
+mkdir -p package/riaps-pycom-armhf/opt/riaps-pycom/
+mkdir -p package/riaps-pycom-armhf/usr/local/riaps/etc/
+mkdir -p package/riaps-pycom-armhf/usr/local/riaps/lang/
+mkdir -p package/riaps-pycom-armhf/usr/local/riaps/keys/
+
 cp -r DEBIAN/armhf/* package/riaps-pycom-armhf/DEBIAN/.
 cp -r src package/riaps-pycom-armhf/opt/riaps-pycom/.
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-armhf/usr/local/riaps/etc/
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-armhf/usr/local/riaps/etc/
+cp -r src/riaps/etc/redis.conf package/riaps-pycom-armhf/usr/local/riaps/etc/
+cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-armhf/usr/local/riaps/etc/
+cp -r src/riaps/lang/riaps.tx package/riaps-pycom-armhf/usr/local/riaps/lang/
+cp -r src/riaps/lang/depl.tx package/riaps-pycom-armhf/usr/local/riaps/lang/
+
+cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-armhf/usr/local/riaps/keys/
+cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-armhf/usr/local/riaps/keys/
+
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-armhf/DEBIAN/control
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-armhf/opt/riaps-pycom/src/setup.py
 fakeroot dpkg-deb --build package/riaps-pycom-armhf
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 export externalsversion="0.7.3"
 export coreversion="0.8.5"
-export pycomversion="0.8.1"
+export pycomversion="0.8.2rc1"
