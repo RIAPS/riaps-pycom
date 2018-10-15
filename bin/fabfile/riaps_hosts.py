ca8b2e41fa102bfb6f631a148f42af6ae13dfca7
Joey Holliday <joeyholliday77@gmail.com>
2018-10-15 16:40:43 -0500

    Update fabfile riaps_hosts.py and README (#115)
    * Remove cluster domain names from riaps_hosts.py
    
    * Update README.md to reflect changes in fabfile/riaps_hosts.py
    
    * Update riaps_hosts.py
    
    * Update README.md

--- bin/fabfile/riaps_hosts.py
+++ bin/fabfile/riaps_hosts.py
@@ -1,17 +1,12 @@
-# riaps_hosts.py - Configuration for RIAPS hosts
-# imported into the fab file
-# Edit this file to match your configuration
+# riaps_hosts.py - Configuration for RIAPS hosts which is imported into __init__.py
 from fabric.api import env
 
 # If a no commandline roles or hosts are passed (i.e. -R or -H), only then use listed hosts
 # Allows for passing of individual hosts or roles on which to run tasks
 if not env.roles and not env.hosts:
-    # List of roles and hosts -- EDIT HERE --
-    env.hosts = ['bbb-2839.local','bbb-164c.local','bbb-e835.local','bbb-ef9f.local',
-                  'bbb-d521.local','bbb-cdee.local','bbb-1180.local','bbb-e22d.local',
-                  'bbb-feb5.local','bbb-4797.local','bbb-eb18.local','bbb-fea3.local',
-                  'bbb-a0a6.local','bbb-923a.local','bbb-be53.local','bbb-da04.local',
-                  'bbb-f913.local','bbb-1d35.local','bbb-e528.local','bbb-93bb.local',
-                  'bbb-e7b8.local','bbb-23c6.local','bbb-f365.local','bbb-8be2.local',
-                  'bbb-e7b9.local','bbb-da61.local','bbb-20fb.local','bbb-4930.local',
-                  'bbb-7030.local','bbb-5df2.local','bbb-1610.local','bbb-da2e.local']
+    
+    # ---- START OF EDIT HERE ----
+    # List of bbb hosts
+    # BBBs can be addressed by their IP address or the hostname.local (found at the command prompt on the BBB)
+    env.hosts = ['192.168.1.2','192.168.1.3','ubuntu.local','bbb-ef9e.local']
+    # ----  END OF EDIT HERE  ----
