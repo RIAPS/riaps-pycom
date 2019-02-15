afef855f28b22c6b98b95e6f56499b822c687582
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-02-14 18:53:10 -0600

    Updating installation to use conffiles for configuration management (still testing)

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@ rm -rf package
 source version.sh
 mkdir -p package/riaps-pycom-amd64/DEBIAN
 mkdir -p package/riaps-pycom-amd64/etc/
+mkdir -p package/riaps-pycom-amd64/etc/apparmor.d/
 mkdir -p package/riaps-pycom-amd64/opt/riaps-pycom/
 mkdir -p package/riaps-pycom-amd64/usr/local/riaps/etc/
 mkdir -p package/riaps-pycom-amd64/usr/local/riaps/lang/
@@ -9,15 +10,13 @@ mkdir -p package/riaps-pycom-amd64/usr/local/riaps/keys/
 
 cp -r DEBIAN/amd64/* package/riaps-pycom-amd64/DEBIAN/.
 cp -r src package/riaps-pycom-amd64/opt/riaps-pycom/.
-
-cp -r src/riaps/etc/riaps.conf package/riaps-pycom-amd64/etc/riaps/riaps.conf.new
-cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-amd64/etc/riaps/riaps-log.conf.new
-
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-amd64/etc/.
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-amd64/etc/.
 cp -r src/riaps/etc/redis.conf package/riaps-pycom-amd64/usr/local/riaps/etc/.
 cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-amd64/usr/local/riaps/etc/.
 cp -r src/riaps/lang/riaps.tx package/riaps-pycom-amd64/usr/local/riaps/lang/.
 cp -r src/riaps/lang/depl.tx package/riaps-pycom-amd64/usr/local/riaps/lang/.
-cp -r src/riaps/etc/usr.local.bin.riaps_actor package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/etc/usr.local.bin.riaps_actor package/riaps-pycom-amd64/etc/apparmor.d/.
 
 cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-amd64/usr/local/riaps/keys/.
 cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-amd64/usr/local/riaps/keys/.
@@ -33,6 +32,7 @@ cp package/riaps-pycom-amd64.deb .
 source version.sh
 mkdir -p package/riaps-pycom-armhf/DEBIAN
 mkdir -p package/riaps-pycom-armhf/etc/
+mkdir -p package/riaps-pycom-armhf/etc/apparmor.d/
 mkdir -p package/riaps-pycom-armhf/opt/riaps-pycom/
 mkdir -p package/riaps-pycom-armhf/opt/riaps-pycom/
 mkdir -p package/riaps-pycom-armhf/usr/local/riaps/etc/
@@ -41,13 +41,12 @@ mkdir -p package/riaps-pycom-armhf/usr/local/riaps/keys/
 
 cp -r DEBIAN/armhf/* package/riaps-pycom-armhf/DEBIAN/.
 cp -r src package/riaps-pycom-armhf/opt/riaps-pycom/.
-cp -r src/riaps/etc/riaps.conf package/riaps-pycom-armhf/etc/riaps/riaps.conf.new
-cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-armhf/etc/riaps/riaps-log.conf.new
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-armhf/etc/.
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-armhf/etc/.
 cp -r src/riaps/etc/redis.conf package/riaps-pycom-armhf/usr/local/riaps/etc/.
-cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-armhf/usr/local/riaps/etc/.
 cp -r src/riaps/lang/riaps.tx package/riaps-pycom-armhf/usr/local/riaps/lang/.
 cp -r src/riaps/lang/depl.tx package/riaps-pycom-armhf/usr/local/riaps/lang/.
-cp -r src/riaps/etc/usr.local.bin.riaps_actor package/riaps-pycom-armhf/etc/riaps/.
+cp -r src/riaps/etc/usr.local.bin.riaps_actor package/riaps-pycom-armhf/etc/apparmor.d/.
 
 cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-armhf/usr/local/riaps/keys/.
 cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-armhf/usr/local/riaps/keys/.
