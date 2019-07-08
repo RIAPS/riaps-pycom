115c3f887069520559f1c3b4f6b72033c108e1f5
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-07-08 12:06:58 -0500

    Install conf file location adjustment

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@ rm -rf package
 source version.sh
 mkdir -p package/riaps-pycom-amd64/DEBIAN
 mkdir -p package/riaps-pycom-amd64/etc/
+mkdir -p package/riaps-pycom-amd64/etc/riaps/
 mkdir -p package/riaps-pycom-amd64/opt/riaps-pycom/
 mkdir -p package/riaps-pycom-amd64/usr/local/riaps/etc/
 mkdir -p package/riaps-pycom-amd64/usr/local/riaps/lang/
@@ -9,17 +10,17 @@ mkdir -p package/riaps-pycom-amd64/usr/local/riaps/lang/
 cp -r DEBIAN/amd64/pkgfiles/* package/riaps-pycom-amd64/DEBIAN/.
 cp -r DEBIAN/amd64/etc/* package/riaps-pycom-amd64/etc/.
 cp -r src package/riaps-pycom-amd64/opt/riaps-pycom/.
-cp -r src/riaps/etc/riaps.conf package/riaps-pycom-amd64/etc/.
-cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-amd64/etc/.
-cp -r src/riaps/etc/riaps-hosts.conf package/riaps-pycom-amd64/etc/.
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/etc/riaps-hosts.conf package/riaps-pycom-amd64/etc/riaps/.
 cp -r src/riaps/etc/redis.conf package/riaps-pycom-amd64/usr/local/riaps/etc/.
 cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-amd64/usr/local/riaps/etc/.
 cp -r src/riaps/lang/riaps.tx package/riaps-pycom-amd64/usr/local/riaps/lang/.
 cp -r src/riaps/lang/depl.tx package/riaps-pycom-amd64/usr/local/riaps/lang/.
-cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-amd64/etc/.
-cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-amd64/etc/.
-cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-amd64/etc/.
-cp -r src/riaps/keys/x509.pem package/riaps-pycom-amd64/etc/.
+cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-amd64/etc/riaps/.
+cp -r src/riaps/keys/x509.pem package/riaps-pycom-amd64/etc/riaps/.
 
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-amd64/DEBIAN/control
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-amd64/opt/riaps-pycom/src/setup.py
@@ -30,6 +31,7 @@ cp package/riaps-pycom-amd64.deb .
 source version.sh
 mkdir -p package/riaps-pycom-armhf/DEBIAN
 mkdir -p package/riaps-pycom-armhf/etc/
+mkdir -p package/riaps-pycom-armhf/etc/riaps/
 mkdir -p package/riaps-pycom-armhf/opt/riaps-pycom/
 mkdir -p package/riaps-pycom-armhf/usr/bin/
 mkdir -p package/riaps-pycom-armhf/usr/local/riaps/etc/
@@ -39,15 +41,15 @@ cp -r DEBIAN/armhf/pkgfiles/* package/riaps-pycom-armhf/DEBIAN/.
 cp -r DEBIAN/armhf/etc/* package/riaps-pycom-armhf/etc/.
 cp -r DEBIAN/armhf/usr/bin/* package/riaps-pycom-armhf/usr/bin/.
 cp -r src package/riaps-pycom-armhf/opt/riaps-pycom/.
-cp -r src/riaps/etc/riaps.conf package/riaps-pycom-armhf/etc/.
-cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-armhf/etc/.
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-armhf/etc/riaps/.
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-armhf/etc/riaps/.
 cp -r src/riaps/etc/redis.conf package/riaps-pycom-armhf/usr/local/riaps/etc/.
 cp -r src/riaps/lang/riaps.tx package/riaps-pycom-armhf/usr/local/riaps/lang/.
 cp -r src/riaps/lang/depl.tx package/riaps-pycom-armhf/usr/local/riaps/lang/.
-cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-armhf/etc/.
-cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-armhf/etc/.
-cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-armhf/etc/.
-cp -r src/riaps/keys/x509.pem package/riaps-pycom-armhf/etc/.
+cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-armhf/etc/riaps/.
+cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-armhf/etc/riaps/.
+cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-armhf/etc/riaps/.
+cp -r src/riaps/keys/x509.pem package/riaps-pycom-armhf/etc/riaps/.
 
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-armhf/DEBIAN/control
 sed s/@version@/$pycomversion/g -i package/riaps-pycom-armhf/opt/riaps-pycom/src/setup.py
