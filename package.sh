080225f9461bb30c9686a119730b2c75b369d0ee
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2020-07-07 16:44:14 -0500

    Move to aarch64 name to work for both Jetson Nano and Raspberry Pi 4

--- package.sh
+++ package.sh
@@ -58,32 +58,32 @@ fakeroot dpkg-deb --build package/riaps-pycom-armhf
 cp package/riaps-pycom-armhf.deb .
 
 source version.sh
-mkdir -p package/riaps-pycom-arm64/DEBIAN
-mkdir -p package/riaps-pycom-arm64/etc/
-mkdir -p package/riaps-pycom-arm64/etc/riaps/
-mkdir -p package/riaps-pycom-arm64/opt/riaps-pycom/
-mkdir -p package/riaps-pycom-arm64/usr/bin/
-mkdir -p package/riaps-pycom-arm64/usr/local/riaps/etc/
-mkdir -p package/riaps-pycom-arm64/usr/local/riaps/lang/
+mkdir -p package/riaps-pycom-aarch64/DEBIAN
+mkdir -p package/riaps-pycom-aarch64/etc/
+mkdir -p package/riaps-pycom-aarch64/etc/riaps/
+mkdir -p package/riaps-pycom-aarch64/opt/riaps-pycom/
+mkdir -p package/riaps-pycom-aarch64/usr/bin/
+mkdir -p package/riaps-pycom-aarch64/usr/local/riaps/etc/
+mkdir -p package/riaps-pycom-aarch64/usr/local/riaps/lang/
 
-cp -r DEBIAN/arm64/pkgfiles/* package/riaps-pycom-arm64/DEBIAN/.
-cp -r DEBIAN/arm64/etc/* package/riaps-pycom-arm64/etc/.
-cp -r DEBIAN/arm64/usr/bin/* package/riaps-pycom-arm64/usr/bin/.
-cp -r src package/riaps-pycom-arm64/opt/riaps-pycom/.
-cp -r src/riaps/etc/riaps.conf package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/etc/riaps-hosts.conf package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/etc/redis.conf package/riaps-pycom-arm64/usr/local/riaps/etc/.
-cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-arm64/usr/local/riaps/etc/.
-cp -r src/riaps/lang/riaps.tx package/riaps-pycom-arm64/usr/local/riaps/lang/.
-cp -r src/riaps/lang/depl.tx package/riaps-pycom-arm64/usr/local/riaps/lang/.
-cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-arm64/etc/riaps/.
-cp -r src/riaps/keys/x509.pem package/riaps-pycom-arm64/etc/riaps/.
+cp -r DEBIAN/aarch64/pkgfiles/* package/riaps-pycom-aarch64/DEBIAN/.
+cp -r DEBIAN/aarch64/etc/* package/riaps-pycom-aarch64/etc/.
+cp -r DEBIAN/aarch64/usr/bin/* package/riaps-pycom-aarch64/usr/bin/.
+cp -r src package/riaps-pycom-aarch64/opt/riaps-pycom/.
+cp -r src/riaps/etc/riaps.conf package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/etc/riaps-log.conf package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/etc/riaps-hosts.conf package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/etc/redis.conf package/riaps-pycom-aarch64/usr/local/riaps/etc/.
+cp -r src/riaps/etc/riaps-ctrl.glade package/riaps-pycom-aarch64/usr/local/riaps/etc/.
+cp -r src/riaps/lang/riaps.tx package/riaps-pycom-aarch64/usr/local/riaps/lang/.
+cp -r src/riaps/lang/depl.tx package/riaps-pycom-aarch64/usr/local/riaps/lang/.
+cp -r src/riaps/keys/id_rsa.key package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/keys/id_rsa.pub package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/keys/riaps-sys.cert package/riaps-pycom-aarch64/etc/riaps/.
+cp -r src/riaps/keys/x509.pem package/riaps-pycom-aarch64/etc/riaps/.
 
-sed s/@version@/$pycomversion/g -i package/riaps-pycom-arm64/DEBIAN/control
-sed s/@version@/$pycomversion/g -i package/riaps-pycom-arm64/opt/riaps-pycom/src/setup.py
-fakeroot dpkg-deb --build package/riaps-pycom-arm64
+sed s/@version@/$pycomversion/g -i package/riaps-pycom-aarch64/DEBIAN/control
+sed s/@version@/$pycomversion/g -i package/riaps-pycom-aarch64/opt/riaps-pycom/src/setup.py
+fakeroot dpkg-deb --build package/riaps-pycom-aarch64
 
-cp package/riaps-pycom-arm64.deb .
+cp package/riaps-pycom-aarch64.deb .
--- version.sh
+++ version.sh
@@ -1,2 +1,2 @@
 export coreversion="1.1.16"
-export pycomversion="1.1.17rc1"
+export pycomversion="1.1.18rc1"
