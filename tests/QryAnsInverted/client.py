da316410e98ab58417e3b965afa5aead8014e6ce
Scott Eisele <eiselesr@gmail.com>
2021-06-29 13:47:43 -0500

    QryAns inverted example

---
+++ tests/QryAnsInverted/client.py
@@ -0,0 +1,56 @@
+# riaps:keep_import:begin
+import time
+
+from riaps.run.comp import Component
+import spdlog
+import capnp
+# import invertQryAns_capnp
+
+# riaps:keep_import:end
+
+class client(Component):
+
+# riaps:keep_constr:begin
+    def __init__(self, name):
+        super(client, self).__init__()
+        self.name = name
+        self.msg_num = 0
+        self.connected = False
+
+
+# riaps:keep_constr:end
+
+# riaps:keep_qry_port:begin
+    def on_qry_port(self):
+        msg = self.qry_port.recv_pyobj()
+        self.logger.info(f"client received answer: {msg}")
+        self.connected = True
+# riaps:keep_qry_port:end
+
+# riaps:keep_poller:begin
+    def on_poller(self):
+        now = self.poller.recv_pyobj()
+        self.logger.info(f"client poll")
+
+        if self.connected:
+            self.poller.deactivate()
+            self.logger.info(f"client connected - Deactivate poller")
+            return None
+
+        self.msg_num += 1
+        msg = {"name": self.name,
+               "status": "ready",
+               "msg_num": self.msg_num}
+        self.qry_port.send_pyobj(msg)
+
+
+
+# riaps:keep_poller:end
+
+# riaps:keep_impl:begin
+
+    def handleActivate(self):
+        self.logger.info(f"client handleActivate()")
+
+
+# riaps:keep_impl:end
