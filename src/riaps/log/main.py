674f6be7c17110a91a0cb5eee650c809982583ee
Scott Eisele <eiselesr@gmail.com>
2022-10-20 17:03:25 +0000

    add shared message queue

--- src/riaps/log/main.py
+++ src/riaps/log/main.py
@@ -4,17 +4,18 @@ Created on Oct 10, 2022
 
 @author: riaps
 """
-import sys
-import os
-import signal
 import argparse
 import multiprocessing
+import os
+import queue
+import signal
+import sys
 import traceback
 
 import riaps.log.server
 from riaps.consts.defs import *
 from riaps.utils.config import Config
-from riaps.log.server import BaseLogServer
+from riaps.log.server import AppLogServer
 from riaps.log.server import PlatformLogServer
 import riaps.log.visualizers.tmux as visualizer
 from riaps.utils.trace import riaps_trace
@@ -72,10 +73,12 @@ def setup(platform, app):
     servers = {}
 
     if platform:
+        q = queue.Queue()
         view = visualizer.View(session_name="platform")
         platform_log_server = PlatformLogServer(server_address=platform,
                                                 RequestHandlerClass=riaps.log.server.PlatformLogHandler,
-                                                view=view)
+                                                view=view,
+                                                q=q)
 
         p = multiprocessing.Process(target=platform_log_server.serve_until_stopped)
         servers["platform"] = {"server": platform_log_server,
@@ -84,10 +87,12 @@ def setup(platform, app):
         p.start()
 
     if app:
+        q = queue.Queue()
         view = visualizer.View(session_name="app")
-        app_log_server = BaseLogServer(server_address=app,
-                                       RequestHandlerClass=riaps.log.server.AppLogHandler,
-                                       view=view)
+        app_log_server = AppLogServer(server_address=app,
+                                      RequestHandlerClass=riaps.log.server.AppLogHandler,
+                                      view=view,
+                                      q=q)
         p = multiprocessing.Process(target=app_log_server.serve_until_stopped)
         servers["app"] = {"server": app_log_server,
                           "process": p,
