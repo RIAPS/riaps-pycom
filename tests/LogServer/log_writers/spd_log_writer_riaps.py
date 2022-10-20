c30ff2b0e96ec5f8ffa9cbac52a3613463d0e535
Scott Eisele <eiselesr@gmail.com>
2022-10-20 17:10:00 +0000

    update spdlog config tester

--- tests/LogServer/log_writers/spd_log_writer_riaps.py
+++ tests/LogServer/log_writers/spd_log_writer_riaps.py
@@ -1,20 +1,26 @@
 import riaps.utils.spdlog_setup as spdlog_setup
+import platform
+import time
 
 loggers = spdlog_setup.from_file("example_configs/riaps-log.conf")
 
 print(f"loggers: {loggers}")
-for logger in loggers:
-    print(f"logger: {logger}")
-    loggers[logger].info("Test Message")
 
-# def tcp_log(log_msg, level):
-#     jsonpattern = f"{{\"time\": \"%E\", " \
-#                   "\"name\": \"%n\", " \
-#                   "\"level\": \"%^%l%$\", " \
-#                   "\"process\": %P," \
-#                   "\"thread\": %t, " \
-#                   "\"message\": \"%v\", " \
-#                   f"\"hostname\": \"{platform.uname().node}\" }}"
+
+def test_loggers(msg):
+    for logger in loggers:
+        print(f"logger: {logger}")
+        loggers[logger].info(msg)
+
+
+def tcp_log(log_msg, level):
+    jsonpattern = f"{{\"time\": \"%E\", " \
+                  "\"name\": \"%n\", " \
+                  "\"level\": \"%^%l%$\", " \
+                  "\"process\": %P," \
+                  "\"thread\": %t, " \
+                  "\"message\": \"%v\", " \
+                  f"\"hostname\": \"{platform.uname().node}\" }}"
 
 #     basic = "message: %v"
 #     pattern = jsonpattern
@@ -23,6 +29,8 @@ for logger in loggers:
 #     getattr(logger, level)(log_msg)
 #
 #
-# for i in range(10):
-#     tcp_log(f"message: {i}", "info")
-#     time.sleep(1)
+
+
+for i in range(10):
+    test_loggers(f"message: {i}")
+    time.sleep(1)
